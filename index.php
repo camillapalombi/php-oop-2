d6510f75eda1b8aa0927a57854b9ab996d901bed
camillapalombi <camilla.palombi14@gmail.com>
2022-05-02 16:40:13 +0200

    Inizio index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-
+// INCLUDE FILE PHP
+include_once __DIR__ . '/User.php';
 include_once __DIR__ . '/Product.php';
 include_once __DIR__ . '/Food.php';
+include_once __DIR__ . '/Accessories.php';
+include_once __DIR__ . '/Care.php';
+
+//USER
+$CamiPalo97 = new User('Camilla', 'Palombi', 'camilla_palombi@yahoo.com');
+
+//FOOD
+$MongeNatural = new Food('Monge Natural', 27,'Dog', 'Monge', 'Calf', 8);
+
+//ACCESSORIES
+$JK9Speed = new Accessories('Julius', 35, 'Cat', 'Julius-k9', 'Harness', 'Walk the cat');
+
 
 ?>
